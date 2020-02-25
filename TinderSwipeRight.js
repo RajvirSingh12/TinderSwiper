ae4ea203a47e62acedaaf47512d8a5909adab83f
RajvirSingh12 <54405659+RajvirSingh12@users.noreply.github.com>
2020-02-25 14:31:31 -0500

    Update TinderSwipeRight.js

--- TinderSwipeRight.js
+++ TinderSwipeRight.js
@@ -1,5 +1,5 @@
 const puppeteer = require("puppeteer");
-const {username, password} = require("./credentials");
+const {username, password} = require("./credentials"); //if you choose to put your login credentials in another file 
 (async ()=>{
     try{
         const browser = await puppeteer.launch({headless: false});//open browser not in background
@@ -9,7 +9,7 @@ const {username, password} = require("./credentials");
         await page.goto("https://tinder.com/");
         await context.overridePermissions("https://tinder.com/", ["geolocation"]);//allow location
 
-        await page.setGeolocation({latitude:####, longitude:#####});//change ur location using coordinates
+        await page.setGeolocation({latitude:####, longitude:####});//change ur location
         await page._client.send("Emulation.clearDeviceMetricsOverride")//fix browser to full window display
         await page.waitForXPath('(//*[@id="modal-manager"]/div/div/div/div/div[3]/div[2]/button)') //use xPath to to find location of FB login button
 
@@ -25,30 +25,31 @@ const {username, password} = require("./credentials");
         await fbPopup.waitForSelector("#email");
         
         await fbPopup.click("#email");//id is email
-        await fbPopup.keyboard.type(username);//enter your username as a string or stored var
+        await fbPopup.keyboard.type(username);
         await fbPopup.click("#pass");//id is pass
-        await fbPopup.keyboard.type(password);//enter your password as a string or stored var
+        await fbPopup.keyboard.type(password);
 
         await fbPopup.waitForSelector("#loginbutton");
         await fbPopup.click("#loginbutton");
 
-        await page.waitForXPath('(//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1])')
-        const [allowLocation] = await page.$x('(//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1])')//allow location 
+        await page.waitForXPath('(//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1])')//allow location
+        const [allowLocation] = await page.$x('(//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1])')
         allowLocation.click();
 
-        await page.waitForXPath('(/html/body/div[2]/div/div/div/div/div[3]/button[1])')
-        const [allowNotifactions] = await page.$x('(/html/body/div[2]/div/div/div/div/div[3]/button[1])')//allow notifactions
-        allowNotifactions.click();
+        await page.waitForXPath('(/html/body/div[2]/div/div/div/div/div[3]/button[1])')//allow notifactions
+        //const [allowNotifactions] = await page.$x('(/html/body/div[2]/div/div/div/div/div[3]/button[1])')
+        //allowNotifactions.click();
+        page.click("[aria-label='Enable']")
 
-        try{
-            await page.waitForXPath('(//*[@id="content"]/div/div[1]/div/div/main/div/div[1]/div/div[1]/div[3]/div[1]/div[1]/div/div[2]/div/div)') //wait for new person to load
+        try{//if there is a person to swipe on swipe right
+            await page.waitForXPath('(//*[@id="content"]/div/div[1]/div/div/main/div/div[1]/div/div[1]/div[3]/div[1]/div[1]/div/div[2]/div/div)')
         }catch(error){
             process.kill();
         }
 
-        setInterval(()=> { //wait time
-            page.click("[aria-label='Like']")//swipe right
-        }, 300);
+        setInterval(()=> {
+            page.click("[aria-label='Like']")
+        }, 8000);//time before swiping (wait time)
     }catch(e){
         process.exit();
     }
